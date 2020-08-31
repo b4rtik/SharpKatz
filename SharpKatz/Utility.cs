aedddf5ac512a424715bf29e6fb4ccf9cba2c0da
Ian Qvist <qvistian@gmail.com>
2020-08-31 20:03:36 +0200

    Remove unused method. We already have PrintHexBytes

--- SharpKatz/Utility.cs
+++ SharpKatz/Utility.cs
@@ -468,14 +468,5 @@ namespace SharpKatz
                 return false;
             }
         }
-
-        public static string ByteArrayToString(byte[] ba)
-        {
-            StringBuilder hex = new StringBuilder(ba.Length * 2);
-            foreach (byte b in ba)
-                hex.AppendFormat("{0:x2}", b);
-            return hex.ToString();
-        }
-
     }
 }
